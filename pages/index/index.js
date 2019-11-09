18f352604c5d7116f6c1c1dbe1998bfcd07b1543
lacxxy <1371214566@qq.com>
2019-11-09 22:48:26 +0800

    网络请求demo

--- pages/index/index.js
+++ pages/index/index.js
@@ -14,14 +14,37 @@ Component({
     }
   },
   ready: function () {
-    /*var that = this;
-    var query = qq.createSelectorQuery();
-    query.select('.work').boundingClientRect(function (res) {
-      console.log(res);
-      that.setData({
-        itemHeight: res.height / 3.5
-      })
-      console.log(that.data)
-    }).exec();*/
+    var that = this;
+    qq.login({
+      success(res) {
+        console.log(res)
+        if (res.code) {
+          // 发起网络请求
+          qq.request({
+            url: 'http://39.108.118.180:8080/session',
+            method: "POST",
+            data: {
+              code: res.code
+            },
+            header: {
+              "Content-Type": "application/x-www-form-urlencoded"
+            },
+          })
+        } else {
+          console.log('登录失败！' + res.errMsg)
+        }
+      }
+    })
+    /*qq.request({
+      url: 'http://39.108.118.180:8080/course',
+      method: "POST",
+      data: {
+        account: '031702635',
+        pwd: '2375468369zh'
+      },
+      success: function (res) {
+        console.log(res)
+      }
+    })*/
   }
 })
